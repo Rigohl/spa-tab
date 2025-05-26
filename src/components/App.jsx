d80a4b81eb1ddf3d97240d85a3047856e7c673f7
Rigohl <rigobertohula@gmail.com>
2025-05-26 15:46:43 -0600

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Kanban from '../views/Kanban';
+import Estadisticas from '../views/Estadisticas';
+
+const App = () => (
+  <div>
+    <h1>CRM Kanban SpaMonterrey funcionando</h1>
+    <Kanban />
+    <Estadisticas />
+  </div>
+);
+
+export default App;
