743cd5196dd5613134f710f4d7c0605c44e3a80d
Rigohl <rigobertohula@gmail.com>
2025-05-26 21:50:44 -0600

    Actualizar Kanban.jsx

--- src/views/Kanban.jsx
+++ src/views/Kanban.jsx
@@ -1,1 +1,36 @@
-// Kanban.jsx - contenido simulado
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { obtenerDatos } from '../utils/googleSheets';
+import Column from '../components/Column';
+
+const columnas = [
+  'Nuevo contacto',
+  'Respondido',
+  'Citada a entrevista',
+  'Acudió',
+  'Negocio asignado',
+  'Dejó de contestar',
+  'Seguimiento'
+];
+
+const Kanban = () => {
+  const [datos, setDatos] = useState([]);
+
+  useEffect(() => {
+    obtenerDatos().then(setDatos);
+  }, []);
+
+  const datosPorColumna = (columna) => {
+    return datos.filter((item) => item.estado === columna);
+  };
+
+  return (
+    <div style={{ display: 'flex', overflowX: 'auto' }}>
+      {columnas.map((col) => (
+        <Column key={col} title={col} cards={datosPorColumna(col)} />
+      ))}
+    </div>
+  );
+};
+
+export default Kanban;
