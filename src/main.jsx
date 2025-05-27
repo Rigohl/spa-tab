609fb4accee2da177a94b51a4a503435ff956ae3
Rigohl <rigobertohula@gmail.com>
2025-05-26 18:24:08 -0600

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App.jsx';
+import './App.css'; // Asegúrate que exista este archivo
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
