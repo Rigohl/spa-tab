15b2aa63740b05ff9f5577e6035bcd11db79a388
Rigohl <rigobertohula@gmail.com>
2025-05-26 21:53:07 -0600

    Actualizar googleSheets.js

--- src/utils/googleSheets.js
+++ src/utils/googleSheets.js
@@ -1,1 +1,15 @@
-// googleSheets.js - contenido simulado
+export const obtenerDatos = async () => {
+  const response = await fetch(
+    'https://sheets.googleapis.com/v4/spreadsheets/11mbwKnaO33uNpTwYtQZ896cTTpWnYNoy27ERXKW4ddg/values/Hoja1?alt=json&key=TU_API_KEY'
+  );
+  const data = await response.json();
+  const [headers, ...rows] = data.values;
+
+  return rows.map((row) => {
+    const obj = {};
+    headers.forEach((header, i) => {
+      obj[header.trim()] = row[i] || '';
+    });
+    return obj;
+  });
+};
