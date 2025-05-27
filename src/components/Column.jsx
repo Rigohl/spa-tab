7bd29febdcb9a50b0c99293ec8dcc75c76548692
Rigohl <rigobertohula@gmail.com>
2025-05-26 21:12:41 -0600

    Actualizar Column.jsx

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,12 +1,10 @@
+// src/components/Column.jsx
 import React from 'react';
 import Card from './Card';
 
 const Column = ({ title, icon, color, cards }) => {
   return (
-    <div
-      className="column"
-      style={{ backgroundColor: color }}
-    >
+    <div className="column" style={{ backgroundColor: color }}>
       <h3>{icon} {title}</h3>
       {cards.length === 0 ? (
         <p style={{ fontStyle: 'italic', color: '#555' }}>Vacío</p>
