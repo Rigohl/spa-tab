b687642952bf36e442c11635727ee16f5585ccf3
Rigohl <rigobertohula@gmail.com>
2025-05-26 21:11:53 -0600

    Actualizar Column.jsx

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,16 +1,18 @@
-// src/components/Column.jsx
 import React from 'react';
 import Card from './Card';
 
 const Column = ({ title, icon, color, cards }) => {
   return (
-    <div className="column" style={{ backgroundColor: color }}>
+    <div
+      className="column"
+      style={{ backgroundColor: color }}
+    >
       <h3>{icon} {title}</h3>
       {cards.length === 0 ? (
         <p style={{ fontStyle: 'italic', color: '#555' }}>Vacío</p>
       ) : (
-        cards.map((card, index) => (
-          <Card key={index} card={card} columnTitle={title} />
+        cards.map((card, idx) => (
+          <Card key={idx} card={card} columnTitle={title} />
         ))
       )}
     </div>
