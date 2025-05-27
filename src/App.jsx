e5441b7212080d0a59f36d79ac6892808ee4e17a
Rigohl <rigobertohula@gmail.com>
2025-05-26 21:49:05 -0600

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-// App.jsx - contenido simulado
+import React from 'react'
+import Kanban from './views/Kanban'
+import Estadisticas from './views/Estadisticas'
+import './App.css'
+
+function App() {
+  const [vista, setVista] = React.useState('kanban')
+
+  return (
+    <div>
+      <nav>
+        <button onClick={() => setVista('kanban')}>CRM</button>
+        <button onClick={() => setVista('stats')}>Estadísticas</button>
+      </nav>
+      {vista === 'kanban' ? <Kanban /> : <Estadisticas />}
+    </div>
+  )
+}
+
+export default App
