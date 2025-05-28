c6fbe091d3f391cb9ac28b340205325143476dbb
Rigohl <rigobertohula@gmail.com>
2025-05-27 22:02:27 -0600

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import Kanban from './views/Kanban'
-import Estadisticas from './views/Estadisticas'
-import './App.css'
+// src/App.jsx
+import React, { useState } from 'react';
+import Kanban from './views/Kanban';
+import Estadisticas from './views/Estadisticas';
+import Login from './views/Login';
+import Header from './components/Header';
+import { obtenerRol } from './utils/roles';
+import './App.css';
 
 function App() {
-  const [vista, setVista] = React.useState('kanban')
+  const [usuario, setUsuario] = useState(null);
+  const [vista, setVista] = useState('kanban');
+
+  const handleLogin = (telefono) => {
+    const rol = obtenerRol(telefono);
+    setUsuario({ telefono, rol });
+  };
+
+  const handleLogout = () => {
+    setUsuario(null);
+  };
+
+  if (!usuario) {
+    return <Login onLogin={handleLogin} />;
+  }
 
   return (
     <div>
+      <Header usuario={usuario} onLogout={handleLogout} />
       <nav>
         <button onClick={() => setVista('kanban')}>CRM</button>
         <button onClick={() => setVista('stats')}>Estadísticas</button>
       </nav>
       {vista === 'kanban' ? <Kanban /> : <Estadisticas />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
